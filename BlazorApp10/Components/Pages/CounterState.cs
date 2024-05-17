77565db54d2d8fcbcb3967338c5859aafa818d5f
akirin <cimakova2005@gmail.com>
2024-05-17 17:33:30 +0300

    Добавьте файлы проекта.

---
+++ BlazorApp10/Components/Pages/CounterState.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp10.Components.Pages
+{
+    public class CustomCounterState
+    {
+        public List<ItemModel> Items { get; set; } = new List<ItemModel>();
+        public string InputValue { get; set; }
+    }
+
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime TimeAdded { get; set; }
+    }
+}
